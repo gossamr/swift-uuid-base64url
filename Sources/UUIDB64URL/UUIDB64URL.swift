4736b98cbc9a75fada6029cfb6f201001d670d15
gossamr <git@gossamr.org>
2024-10-11 01:12:09 +0200

    fix: UUID extension internal by default

--- Sources/UUIDB64URL/UUIDB64URL.swift
+++ Sources/UUIDB64URL/UUIDB64URL.swift
@@ -11,7 +11,7 @@ extension Data {
     }
 }
 
-extension UUID {
+public extension UUID {
     internal var bytes : [UInt8] {
         let (u1,u2,u3,u4,u5,u6,u7,u8,u9,u10,u11,u12,u13,u14,u15,u16) = self.uuid
         return [u1,u2,u3,u4,u5,u6,u7,u8,u9,u10,u11,u12,u13,u14,u15,u16]
